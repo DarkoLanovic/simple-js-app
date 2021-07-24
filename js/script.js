604b4512b41eacee2a7345234e11d4e220b4c7f8
Darko Lanovic <lanovic.darko@gmail.com>
2021-07-24 18:39:07 -0400

    .DS_Store
    This commit will have change

---
+++ js/script.js
@@ -0,0 +1,29 @@
+alert('Hello World');
+
+let favoriteFood = 'Mediterranean';
+document.write(favoriteFood);
+
+let myName = 'Darko';
+document.write(myName);
+myName = 'Lanovic';
+document.write(myName);
+
+// Math exsmles
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+// STRINGS
+let text1 = 'This is a text!';
+let text2 = "This uses double quotes.";
+
+let escapedText = "He said: \"Yes!\", that's for sure";
+let escapedText2 = 'He said: "Yes!", that\'s for sure.';
+// If I want to use backslash in text
+let escapedText3 = 'This is \\ it';

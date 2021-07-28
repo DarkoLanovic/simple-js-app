2055b66e507ba619c7c83161491543fd0941ad22
Darko Lanovic <lanovic.darko@gmail.com>
2021-07-28 11:06:51 -0400

    Update script.js
    Change

--- js/script.js
+++ js/script.js
@@ -1,30 +1,64 @@
-alert('Hello, My name is Darko');
 
 
-let favoriteFood = 'Mediterranean';
-document.write(favoriteFood);
 
-let myName = 'Darko';
-document.write(myName);
-myName = 'Lanovic';
-document.write(myName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert('Hello, My name is Darko');
+
+//let favoriteFood = 'Mediterranean';
+//document.write(favoriteFood);
+
+//let myName = 'Darko';
+//document.write(myName);
+//myName = 'Lanovic';
+//document.write(myName);
 
 // Math exsmles
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
+//let simpleAddition = 2 + 2;
+//document.write(simpleAddition);
 
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
+//let size = 100;
+//let doubleSize = size * 2;
+//document.write(doubleSize);
 
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+//let minSize = (doubleSize * 2) - (size / 2);
+//document.write(minSize);
 
 // STRINGS
-let text1 = 'This is a text!';
-let text2 = "This uses double quotes.";
+//let text1 = 'This is a text!';
+//let text2 = "This uses double quotes.";
 
-let escapedText = "He said: \"Yes!\", that's for sure";
-let escapedText2 = 'He said: "Yes!", that\'s for sure.';
+//let escapedText = "He said: \"Yes!\", that's for sure";
+//let escapedText2 = 'He said: "Yes!", that\'s for sure.';
 // If I want to use backslash in text
-let escapedText3 = 'This is \\ it';
+//let escapedText3 = 'This is \\ it';

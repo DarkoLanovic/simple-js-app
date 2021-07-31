770a30687ec904943ac66d19273f6fbb40bb08e7
Darko Lanovic <lanovic.darko@gmail.com>
2021-07-30 21:37:24 -0400

    Update script.js
    This commit will have create for loop

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 
-let pokemonList = [{name: "Ivysaur", heigh: "1", type: ["grass","poison"]},
-                   {name: "Spearow", heigh: "0.3", type: ["flying","normal"]},
-                   {name: "Rhydon", heigh: "1.9", type: ["rock","ground"]},
-                   {name: "Starmie", heigh: "1.1", type: ["psychic","water"]},
-                   {name: "Articuno", heigh: "1.7", type: ["ice","flying"]},
-                   {name: "Aggron", heigh: "2.1", type: ["steel","rock"]}
-                  ];
+let pokemonList = [
+  {name: "Ivysaur", heigh: "1", type: ["grass","poison"]},
+  {name: "Spearow", heigh: "0.3", type: ["flying","normal"]},
+  {name: "Rhydon", heigh: "1.9", type: ["rock","ground"]},
+  {name: "Starmie", heigh: "1.1", type: ["psychic","water"]},
+  {name: "Articuno", heigh: "1.7", type: ["ice","flying"]},
+  {name: "Aggron", heigh: "2.1", type: ["steel","rock"]}
+];
+
+for (let i=0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name  + " " + (pokemonList[i].heigh);
+}
+
+
+
+let person = [
+  {name: "person1", age: 16},
+  {name: "person2", age: 10},
+  {name: "person3", age: 25}
+];
+
+for (let i=0; i < person.length; i++){
+  if (person[i].age <19 && person[i].age >13){
+    console.log(person[i].name + " is a teenager");
+  }else if (person[i].age <13){
+    console.log(person[i].name + " is a child");
+  }else {
+    console.log(person[i].name + " is an adult");
+  }
+}
+
 
 
 

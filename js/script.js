1075abc054350f5050c210abf259f130cc5c558c
Darko Lanovic <lanovic.darko@gmail.com>
2021-07-30 22:37:27 -0400

    Update script.js
    This commit will have delete extra code.

--- js/script.js
+++ js/script.js
@@ -9,26 +9,10 @@ let pokemonList = [
 ];
 
 for (let i=0; i < pokemonList.length; i++){
-  document.write(pokemonList[i].name + " " + (pokemonList[i].heigh);
+  document.write('"' + pokemonList[i].name + ' ' + ' (heigh: ' + pokemonList[i].heigh + ')' + '"');
 }
 
 
-
-let person = [
-  {name: "person1", age: 16},
-  {name: "person2", age: 10},
-  {name: "person3", age: 25}
-];
-
-for (let i=0; i < person.length; i++){
-  if (person[i].age <19 && person[i].age >13){
-    console.log(person[i].name + " is a teenager");
-  }else if (person[i].age <13){
-    console.log(person[i].name + " is a child");
-  }else {
-    console.log(person[i].name + " is an adult");
-  }
-}
 
 
 

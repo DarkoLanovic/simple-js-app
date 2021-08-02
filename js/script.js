f99fc1d723dc54818e4e93eec0b86497e4e525c1
Darko Lanovic <lanovic.darko@gmail.com>
2021-08-02 04:29:17 -0400

    Update script.js
    This commit will have do test.

--- js/script.js
+++ js/script.js
@@ -43,7 +43,22 @@ for (let i=0; i < pokemonList.length; i++){
   }
 }
 
+let pokemonRepository = (function () {
+  let pokemonList = []; // empty array
 
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
+
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: 'Pikachu' });
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
 
 
 

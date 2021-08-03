de130e568000089804b8207ad875333c79f29ffc
Darko Lanovic <lanovic.darko@gmail.com>
2021-08-03 13:36:14 -0400

    Update script.js
    This commit will have change "add" function.

--- js/script.js
+++ js/script.js
@@ -34,7 +34,9 @@ let pokemonRepository = (function () {
     ];
 
     function add(pokemon) {
-      pokemonList.push(pokemon);
+      if (typeof pokemon === 'object') {
+        pokemonList.push(pokemon);
+      }
     }
 
     function getAll() {
@@ -49,16 +51,17 @@ let pokemonRepository = (function () {
 
   document.write( pokemonRepository.getAll() );
 
-    // "forEach" loop which iterate over each object within that array and display them in order with their Name and Heigh
-    pokemonRepository.forEach(function(item) {
-      // checking which pokemon has the biggest heigh
-      if (item.heigh >2){
-        document.write('<p> "' + item.name + ' (heigh: ' + item.heigh + ')' + '"' + ' - Wow, that\'s the big one!' + '</p>')
-      }else {
-        document.write(`<p>"${item.name} (heigh: ${item.heigh})"</p>`);
-        //document.write('<p>' + ' "' + pokemonList[i].name + ' ' + ' (heigh: ' + pokemonList[i].heigh + ')' + '"' + '</p>');
-      }
-    });
+
+  // "forEach" loop which iterate over each object within that array and display them in order with their Name and Heigh
+  pokemonRepository.forEach(function(item) {
+    // checking which pokemon has the biggest heigh
+    if (item.heigh >2){
+      document.write('<p> "' + item.name + ' (heigh: ' + item.heigh + ')' + '"' + ' - Wow, that\'s the big one!' + '</p>')
+    }else {
+      document.write(`<p>"${item.name} (heigh: ${item.heigh})"</p>`);
+      //document.write('<p>' + ' "' + pokemonList[i].name + ' ' + ' (heigh: ' + pokemonList[i].heigh + ')' + '"' + '</p>');
+    }
+  });
 
 
 
@@ -92,32 +95,32 @@ let pokemonRepository = (function () {
 
 
 
-    //alert('Hello, My name is Darko');
+  //alert('Hello, My name is Darko');
 
-    //let favoriteFood = 'Mediterranean';
-    //document.write(favoriteFood);
+  //let favoriteFood = 'Mediterranean';
+  //document.write(favoriteFood);
 
-    //let myName = 'Darko';
-    //document.write(myName);
-    //myName = 'Lanovic';
-    //document.write(myName);
+  //let myName = 'Darko';
+  //document.write(myName);
+  //myName = 'Lanovic';
+  //document.write(myName);
 
-    // Math exsmles
-    //let simpleAddition = 2 + 2;
-    //document.write(simpleAddition);
+  // Math exsmles
+  //let simpleAddition = 2 + 2;
+  //document.write(simpleAddition);
 
-    //let size = 100;
-    //let doubleSize = size * 2;
-    //document.write(doubleSize);
+  //let size = 100;
+  //let doubleSize = size * 2;
+  //document.write(doubleSize);
 
-    //let minSize = (doubleSize * 2) - (size / 2);
-    //document.write(minSize);
+  //let minSize = (doubleSize * 2) - (size / 2);
+  //document.write(minSize);
 
-    // STRINGS
-    //let text1 = 'This is a text!';
-    //let text2 = "This uses double quotes.";
+  // STRINGS
+  //let text1 = 'This is a text!';
+  //let text2 = "This uses double quotes.";
 
-    //let escapedText = "He said: \"Yes!\", that's for sure";
-    //let escapedText2 = 'He said: "Yes!", that\'s for sure.';
-    // If I want to use backslash in text
-    //let escapedText3 = 'This is \\ it';
+  //let escapedText = "He said: \"Yes!\", that's for sure";
+  //let escapedText2 = 'He said: "Yes!", that\'s for sure.';
+  // If I want to use backslash in text
+  //let escapedText3 = 'This is \\ it';

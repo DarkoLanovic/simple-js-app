c55b76a41993c73d55c245ae0707f62073f0cfef
Darko Lanovic <lanovic.darko@gmail.com>
2021-08-23 09:04:57 -0400

    js/script.js
    This commit will have create  Modal function and buttons

--- js/script.js
+++ js/script.js
@@ -4,22 +4,6 @@ let pokemonRepository = (function () {
 
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-
-  function add(pokemon) {
-    if (
-      typeof pokemon === "object" &&
-      "name" in pokemon
-    ) {
-      pokemonList.push(pokemon);
-    } else {
-      console.log("pokemon is not correct");
-    }
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
@@ -33,7 +17,7 @@ let pokemonRepository = (function () {
     });
   }
 
-  // Function for load data from an external source
+  // Function for load data from an external source API
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -64,6 +48,57 @@ let pokemonRepository = (function () {
     }).catch(function (e) {
       console.error(e);
     });
+  }
+
+  // MODAL FUNCTIONS
+
+  function showModal(title,text) {
+    let modalContainer = document.querySelector('#modal-container');
+
+    //Clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    //Add new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', () =>{
+    showModal();
+  });
+
+
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
   }
 
   function showDetails(item) {

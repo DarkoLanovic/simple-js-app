08df4eabf1cc0875ca0f0dbd11a93a9c2d5ee04f
Darko Lanovic <lanovic.darko@gmail.com>
2021-09-15 09:22:12 -0400

    Update script.js
    This commit will do update.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-
 const pokemonRepository = (function () {
-
   function getAll() {
-
-    const apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    const apiURL = "https://pokeapi.co/api/v2/pokemon/?limit=150";
     let pokemonList = [];
 
     fetch(apiURL)
@@ -11,25 +8,30 @@ const pokemonRepository = (function () {
       .then((response) => {
         pokemonList = response.results;
         pokemonList.forEach((pokemon) => {
-          const newPokemonList = document.querySelector('.pokemon-list');
+          const newPokemonList = document.querySelector(".pokemon-list");
           // newPokemonList.classList.add("list-group");
-          const listItemPokemon = document.createElement('li');
-          const button = document.createElement('button');
+          const listItemPokemon = document.createElement("li");
+          const button = document.createElement("button");
           button.innerText = pokemon.name;
-          button.classList.add('btn');
-          button.classList.add('button-class');
-          button.setAttribute('data-toggle', 'modal');
-          button.setAttribute('data-target', '#exampleModal');
-          button.addEventListener('click', () => {
+          button.classList.add("btn");
+          button.classList.add("button-class");
+          button.setAttribute("data-toggle", "modal");
+          button.setAttribute("data-target", "#exampleModal");
+          button.addEventListener("click", () => {
             fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
               .then((res) => res.json())
               .then((result) => {
-                const pokemonDetails = `Height: ${result.height}<br>` + `Weight: ${result.weight}<br>`;
+                const pokemonDetails =
+                  `Height: ${result.height}<br>` +
+                  `Weight: ${result.weight}<br>`;
                 //Show modal starts
-                $('.modal-title').html(pokemon.name);
-                $('.modal-body').html('');
+                $(".modal-title").html(pokemon.name);
+                $(".modal-body").html("");
                 // Show modal ends
-                $('.modal-body').append(`${pokemonDetails}`, `<img src="${result.sprites.front_shiny}">`);
+                $(".modal-body").append(
+                  `${pokemonDetails}`,
+                  `<img src="${result.sprites.front_shiny}">`
+                );
               })
               .catch((err) => console.error(err));
 
@@ -42,16 +44,15 @@ const pokemonRepository = (function () {
       .catch((error) => console.log(error));
   }
 
-  function loadcreator()
-  {
-    $('#creator').click(()=>{
-      alert("Created by Darko!")
-    })
+  function loadcreator() {
+    $("#creator").click(() => {
+      alert("Created by Darko!");
+    });
   }
 
   function main() {
     pokemonRepository.getAll();
-    loadcreator()
+    loadcreator();
   }
 
   // adding the return statement
@@ -59,6 +60,6 @@ const pokemonRepository = (function () {
     main,
     getAll,
   };
-}());
+})();
 
 pokemonRepository.main();
